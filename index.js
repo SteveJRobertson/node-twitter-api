619eecbc462a4dbcc998592c0a4841aa77627a97
SteveJRobertson <steve@steverobertson.net>
2015-10-13 09:28:30 +0100

    API files added

---
+++ index.js
@@ -0,0 +1,55 @@
+(function(){
+    var Twitter = require('twitter');
+    var express = require('express');
+    var app = express();
+    var method = 'search/tweets';
+
+    var client = new Twitter({
+        consumer_key: process.env.TWITTER_CONSUMER_KEY,
+        consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+        access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+        access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+    });
+
+    var methods = [
+        'statuses/mentions_timeline',
+        'statuses/home_timeline',
+        'statuses/retweets_of_me',
+        'search/tweets',
+        'lists/statuses',
+        'statuses/retweets/:id',
+    ];
+
+    var twitterAPICall = function(method) {
+        return function(req, res) {
+            var params = req.query;
+            
+            if (Object.keys(req.params).length === 1) {
+                method = method.replace(/:id/, req.params.id);
+            }
+
+            client.get(method, params, function(error, tweets, response) {
+                res.writeHead(response.toString(), {"Content-type": "application/json"});
+                var json = {};
+                if (error) {
+                    json = JSON.stringify(error);
+                } else {
+                    json = JSON.stringify(tweets);
+                }
+                res.write(json);
+                res.end();
+            });
+        }
+    }
+
+    for (var i in methods) {
+        app.get('/' + methods[i], twitterAPICall(methods[i]));
+    }
+
+    var server = app.listen(8081, function() {
+        var host = server.address().address,
+            port = server.address().port;
+
+            console.log("Twitter API app listening at http://%s:%s", host, port);
+    });
+})();
