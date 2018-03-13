b45478a9495cb9bcea9fc1d688ccf9243df59ed7
Steve Robertson <steve@sr.digital>
2018-03-13 11:21:36 +0000

    Updated allowedOrigins

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
     }));
 
     app.use(function (req, res, next) {
-        var allowedOrigins = ['http://localhost:3000', 'http://sr.digital'];
+        var allowedOrigins = ['http://localhost:3000', 'http://sr.digital', 'https://jamdodger-*.herokuapp.com'];
         var origin = req.headers.origin;
         if(allowedOrigins.indexOf(origin) > -1){
             res.setHeader('Access-Control-Allow-Origin', origin);
