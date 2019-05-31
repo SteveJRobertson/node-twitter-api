a01c9935594739209bd7efced161b74b60431a55
Steve Robertson <steve@sr.digital>
2019-05-31 10:56:22 +0100

    Revert previous commit

--- index.js
+++ index.js
@@ -18,7 +18,7 @@
     }));
 
     app.use(function (req, res, next) {
-        var allowedOrigins = ['http://localhost:3000', 'http://sr.digital', 'https://sr.digital', 'https://jamdodger-*.herokuapp.com', 'https://s.codepen.io/SteveJRobertson'];
+        var allowedOrigins = ['http://localhost:3000', 'http://sr.digital', 'https://sr.digital', 'https://jamdodger-*.herokuapp.com', 'https://s.codepen.io'];
         var origin = req.headers.origin;
         if(allowedOrigins.indexOf(origin) > -1){
             res.setHeader('Access-Control-Allow-Origin', origin);
